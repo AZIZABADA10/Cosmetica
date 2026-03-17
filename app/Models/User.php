c20ef50bcb917a3670b6ba42d574baa0a743e4df
AZIZABADA10 <abadaaziz288@gmail.com>
2026-03-17 16:20:05 +0000

    feat(model): mise à jour du modèle User pour JWT et rôles

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,22 +7,38 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    /** @use HasFactory<UserFactory> */
+    use HasApiTokens, HasFactory, Notifiable;
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['name', 'email', 'password', 'role_id'];
+
+
+    public function role() {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function orders() {
+        return $this->hasMany(Order::class);
+    }
+
 
     /**
      * The attributes that should be hidden for serialization.
