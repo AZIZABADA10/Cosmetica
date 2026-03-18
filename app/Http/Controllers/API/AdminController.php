f5f7c20c074c01fc463660c03d3a77bb4fcdfc2e
AZIZABADA10 <abadaaziz288@gmail.com>
2026-03-18 13:26:52 +0000

    feat: ajout de AdminController avec renderUserEmployee

---
+++ app/Http/Controllers/API/AdminController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function getAllUsers()
+    {
+        $users = User::all();
+        return response()->json(['message' =>'tout les utilisateurs','users'=> $users],200);
+    }
+
+    public function getUserById($id)
+    {
+        $user = User::findOrFail($id);
+        return response()->json([$user],200);
+    }
+
+    public function renderUserEmployee($id)
+    {
+        $user = User::findOrFail($id);
+        $role = Role::where('name','employe')->firstOrFail();
+        $user->role_id = $role->id;
+        $user->save();
+        return response()->json([
+            'message'=> 'le role a été changer avec succes',
+            'role'=>$user->load('role')
+            ]);
+    }
+}
